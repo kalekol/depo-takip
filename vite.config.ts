fa80ed10e11440cf9f07582eaad0715a3054cfe3
kalekol <eekinci219@gmail.com>
2026-08-11 18:04:59 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/',
+    base: '/depo-takip/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
